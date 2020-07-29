d539f8da92250e918ed32e0fc34187260bead928
Chana Kupetz <chanakupetz@gmail.com>
2020-07-29 04:27:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,18 +4,14 @@ def find_even_values(src)
   while row_index < src.count do
     element_index = 0
 
+    while element_index < src[row_index].count do 
+        if src[row_index][element_index].even?
+          p src[row_index][element_index]
+     end
 
-
-    
-    while element_index < src[row_index].length do
-      if src[row_index][element_index] %2 == 0
-      puts  src[row_index][element_index]
-      end
-      # %2 == 0
       element_index += 1
     end
 
     row_index += 1
   end
-find_even_values(array_of_arrays)
 end
